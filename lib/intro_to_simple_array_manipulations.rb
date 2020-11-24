0af13b670b8a2e24e8ba46bd18ec83638b1acf70
Marty Warfel <marthawarfel@gmail.com>
2020-11-24 21:21:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,6 @@ def using_push(array, string)
   array.push(string)
 end
 
-
 def using_unshift(array, string)
   array.unshift(string)
 end
@@ -24,22 +23,4 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-
-=begin
-  10) shift_with_args takes in an argument of an array and uses the shift method with an argument of 2 to remove and return the first 2 items from the array
-      Failure/Error: @brands_removed = shift_with_args(@ice_cream_brands)
-
-      NoMethodError:
-        undefined method `shift_with_args' for #<RSpec::ExampleGroups::ShiftWithArgs:0x0000000002aefe78>
-      # ./spec/intro_to_simple_array_manipulations_spec.rb:74:in `block (2 levels) in <top (required)>'
-=end
-
-=begin
-  11) shift_with_args decreases the length of the array by 2
-      Failure/Error: @brands_removed = shift_with_args(@ice_cream_brands)
-
-      NoMethodError:
-        undefined method `shift_with_args' for #<RSpec::ExampleGroups::ShiftWithArgs:0x0000000002aee488>
-      # ./spec/intro_to_simple_array_manipulations_spec.rb:74:in `block (2 levels) in <top (required)>'
-=end 
 
