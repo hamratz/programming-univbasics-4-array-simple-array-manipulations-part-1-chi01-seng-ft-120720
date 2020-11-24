1f98454e7458cf4d198f9e6be8b76dd674afe6fa
Marty Warfel <marthawarfel@gmail.com>
2020-11-24 21:09:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,7 +13,7 @@ end
      # ./spec/intro_to_simple_array_manipulations_spec.rb:17:in `block (2 levels) in <top (required)>'
 =end
 
-def using_upshift(array, string)
+def using_unshift(array, string)
   array.unshift(string)
 end
 
