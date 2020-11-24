ad80b928a9fe1cdc90506b2d2d5a25e43842b2d7
Marty Warfel <marthawarfel@gmail.com>
2020-11-24 21:21:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,23 +20,10 @@ def using_shift(array)
   array.shift
 end
   
-=begin
-  8) using_shift takes in an argument of an array and uses the shift method to remove the first item and return it
-     Failure/Error: @im_so_over_this_city = using_shift(@my_favorite_cities)
+def shift_with_args(array)
+  array.shift(2)
+end
 
-     NoMethodError:
-       undefined method `using_shift' for #<RSpec::ExampleGroups::UsingShift:0x0000000002ae3448>
-     # ./spec/intro_to_simple_array_manipulations_spec.rb:60:in `block (2 levels) in <top (required)>'
-=end
-
-=begin
-  9) using_shift decreases the length of the array by 1
-     Failure/Error: @im_so_over_this_city = using_shift(@my_favorite_cities)
-
-     NoMethodError:
-       undefined method `using_shift' for #<RSpec::ExampleGroups::UsingShift:0x0000000002ae2138>
-     # ./spec/intro_to_simple_array_manipulations_spec.rb:60:in `block (2 levels) in <top (required)>'
-=end
 
 =begin
   10) shift_with_args takes in an argument of an array and uses the shift method with an argument of 2 to remove and return the first 2 items from the array
